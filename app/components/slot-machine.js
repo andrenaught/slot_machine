47ad444047e04a5ef3156da0abceafa97aeeaa4b
KimSaeHun <kimsaehun@csu.fullerton.edu>
2018-05-16 16:55:05 -0700

    Add text to game page and prep for post-win action

--- app/components/slot-machine.js
+++ app/components/slot-machine.js
@@ -8,7 +8,8 @@ let RESULTS = [];
 const NUMBER_OF_REELS = 9;
 
 export default Component.extend({
-  selectedLevel: 1,
+  credits: 0,
+  selectedLevel: 3,
 
   // At this point jQuery onWindow load has completed
   // Calling this makes sure that only 3 slot reel icons are showing
@@ -35,34 +36,33 @@ export default Component.extend({
       if (this.selectedLevel == 1 && ((RESULTS[3] == RESULTS[4]) && (RESULTS[5] == RESULTS[4])))
       {
         if (RESULTS[3] == 0 || RESULTS[3] == 1 || RESULTS[3] == 2)
-          console.log('Jackpot. Won 100');
+          this.winCRedits(100);
         else if (RESULTS[3] == 3 || RESULTS[3] == 4)
-          console.log('Super Jackpot. Won 500');
+          this.winCRedits(300);
         else if (RESULTS[3] == 5)
-          console.log('Mega Jackpot. Won 1000');
+          this.winCRedits(1000);
       }
       // level two
       else if (this.selectedLevel == 2 && (((RESULTS[0] == RESULTS[1]) && (RESULTS[1] == RESULTS[2])) || ((RESULTS[3] == RESULTS[4]) && (RESULTS[4] == RESULTS[5])) || ((RESULTS[6] == RESULTS[7]) && (RESULTS[7] == RESULTS[8]))))
       {
         if (((RESULTS[0] == 0) && (RESULTS[1] == 0) && (RESULTS[2] == 0)) || ((RESULTS[0] == 1) && (RESULTS[1] == 1) && (RESULTS[2] == 1)) || ((RESULTS[0] == 2) && (RESULTS[1] == 2) && (RESULTS[2] == 2)))
-        console.log('Jackpot. Won 100');
-        //Add 100
+          this.winCRedits(100);
         if (((RESULTS[3] == 0) && (RESULTS[4] == 0) && (RESULTS[5] == 0)) || ((RESULTS[3] == 1) && (RESULTS[4] == 1) && (RESULTS[5] == 1)) || ((RESULTS[3] == 2) && (RESULTS[4] == 2) && (RESULTS[5] == 2)))
-          console.log('Jackpot. Won 100');
+          this.winCRedits(100);
         if (((RESULTS[6] == 0) && (RESULTS[7] == 0) && (RESULTS[8] == 0)) || ((RESULTS[6] == 1) && (RESULTS[7] == 1) && (RESULTS[8] == 1)) || ((RESULTS[6] == 2) && (RESULTS[7] == 2) && (RESULTS[8] == 2)))
-          console.log('Jackpot. Won 100');
+          this.winCRedits(100);
         if (((RESULTS[0] == 3) && (RESULTS[1] == 3) && (RESULTS[2] == 3)) || ((RESULTS[0] == 4) && (RESULTS[1] == 4) && (RESULTS[2] == 4)))
-          console.log('Super Jackpot. Won 300');
+          this.winCRedits(300);
         if (((RESULTS[3] == 3) && (RESULTS[4] == 3) && (RESULTS[5] == 3)) || ((RESULTS[3] == 4) && (RESULTS[4] == 4) && (RESULTS[5] == 4)))
-          console.log('Super Jackpot. Won 300');
+          this.winCRedits(300);
         if (((RESULTS[6] == 3) && (RESULTS[7] == 3) && (RESULTS[8] == 3)) || ((RESULTS[6] == 4) && (RESULTS[7] == 4) && (RESULTS[8] == 4)))
-          console.log('Super Jackpot. Won 300');
+          this.winCRedits(300);
         if ((RESULTS[0] == 5) && (RESULTS[1] == 5) && (RESULTS[2] == 5))
-          console.log('Mega Jackpot. Won 1000');
+          this.winCRedits(1000);
         if ((RESULTS[3] == 5) && (RESULTS[4] == 5) && (RESULTS[5] == 5))
-          console.log('Mega Jackpot. Won 1000');
+          this.winCRedits(1000);
         if ((RESULTS[6] == 5) && (RESULTS[7] == 5) && (RESULTS[8] == 5))
-          console.log('Mega Jackpot. Won 1000');
+          this.winCRedits(1000);
       }
       // level three
       else if (this.selectedLevel == 3 && (
@@ -73,42 +73,48 @@ export default Component.extend({
         ((RESULTS[2] == RESULTS[4]) && (RESULTS[2] == RESULTS[6]))))
       {
         if (((RESULTS[0] == 0) && (RESULTS[1] == 0) && (RESULTS[2] == 0)) || ((RESULTS[0] == 1) && (RESULTS[1] == 1) && (RESULTS[2] == 1)) || ((RESULTS[0] == 2) && (RESULTS[1] == 2) && (RESULTS[2] == 2)))
-          console.log('Jackpot. Won 100');
-          //Add 100
+          this.winCRedits(100);
         if (((RESULTS[3] == 0) && (RESULTS[4] == 0) && (RESULTS[5] == 0)) || ((RESULTS[3] == 1) && (RESULTS[4] == 1) && (RESULTS[5] == 1)) || ((RESULTS[3] == 2) && (RESULTS[4] == 2) && (RESULTS[5] == 2)))
-          console.log('Jackpot. Won 100');
+          this.winCRedits(100);
         if (((RESULTS[6] == 0) && (RESULTS[7] == 0) && (RESULTS[8] == 0)) || ((RESULTS[6] == 1) && (RESULTS[7] == 1) && (RESULTS[8] == 1)) || ((RESULTS[6] == 2) && (RESULTS[7] == 2) && (RESULTS[8] == 2)))
-          console.log('Jackpot. Won 100');
+          this.winCRedits(100);
         if (((RESULTS[0] == 0) && (RESULTS[4] == 0) && (RESULTS[8] == 0)) || ((RESULTS[0] == 1) && (RESULTS[4] == 1) && (RESULTS[8] == 1)) || ((RESULTS[0] == 2) && (RESULTS[4] == 2) && (RESULTS[8] == 2)))
-          console.log('Jackpot. Won 100');
+          this.winCRedits(100);
         if (((RESULTS[2] == 0) && (RESULTS[4] == 0) && (RESULTS[6] == 0)) || ((RESULTS[2] == 1) && (RESULTS[4] == 1) && (RESULTS[6] == 1)) || ((RESULTS[2] == 2) && (RESULTS[4] == 2) && (RESULTS[6] == 2)))
-          console.log('Jackpot. Won 100');
+          this.winCRedits(100);
         if (((RESULTS[0] == 3) && (RESULTS[1] == 3) && (RESULTS[2] == 3)) || ((RESULTS[0] == 4) && (RESULTS[1] == 4) && (RESULTS[2] == 4)))
-          console.log('Super Jackpot. Won 300');
+          this.winCRedits(300);
         if (((RESULTS[3] == 3) && (RESULTS[4] == 3) && (RESULTS[5] == 3)) || ((RESULTS[3] == 4) && (RESULTS[4] == 4) && (RESULTS[5] == 4)))
-          console.log('Super Jackpot. Won 300');
+          this.winCRedits(300);
         if (((RESULTS[6] == 3) && (RESULTS[7] == 3) && (RESULTS[8] == 3)) || ((RESULTS[6] == 4) && (RESULTS[7] == 4) && (RESULTS[8] == 4)))
-          console.log('Super Jackpot. Won 300');
+          this.winCRedits(300);
         if (((RESULTS[0] == 3) && (RESULTS[4] == 3) && (RESULTS[8] == 3)) || ((RESULTS[0] == 4) && (RESULTS[4] == 4) && (RESULTS[8] == 4)))
-          console.log('Super Jackpot. Won 300');
+          this.winCRedits(300);
         if (((RESULTS[2] == 3) && (RESULTS[4] == 3) && (RESULTS[6] == 3)) || ((RESULTS[2] == 4) && (RESULTS[4] == 4) && (RESULTS[6] == 4)))
-          console.log('Super Jackpot. Won 300');
+          this.winCRedits(300);
         if ((RESULTS[0] == 5) && (RESULTS[1] == 5) && (RESULTS[2] == 5))
-          console.log('Mega Jackpot. Won 1000');
+          this.winCRedits(1000);
         if ((RESULTS[3] == 5) && (RESULTS[4] == 5) && (RESULTS[5] == 5))
-          console.log('Mega Jackpot. Won 1000');
+          this.winCRedits(1000);
         if ((RESULTS[6] == 5) && (RESULTS[7] == 5) && (RESULTS[8] == 5))
-          console.log('Mega Jackpot. Won 1000');
+          this.winCRedits(1000);
         if ((RESULTS[0] == 5) && (RESULTS[4] == 5) && (RESULTS[8] == 5))
-          console.log('Mega Jackpot. Won 1000');
+          this.winCRedits(1000);
         if ((RESULTS[2] == 5) && (RESULTS[4] == 5) && (RESULTS[6] == 5))
-          console.log('Mega Jackpot. Won 1000');
+          this.winCRedits(1000);
       }
     }
   },
-
+  winCredits(amount) {
+    // add credits to backend
+    for (i = 0; i < amount; i++) {
+      this.credits += 1;
+    }
+  },
   unhighlightAllLevelButtons() {
-    this.$('.slot-container button').css('background-color', 'rgba(0, 0, 0, 0.12)');
+    this.$('.level-one-container button').css('background-color', 'rgba(0, 0, 0, 0.12)');
+    this.$('.level-two-container button').css('background-color', 'rgba(0, 0, 0, 0.12)');
+    this.$('.level-three-container button').css('background-color', 'rgba(0, 0, 0, 0.12)');
   },
   highlightLevelButtons(level) {
     if (level == 1) {
