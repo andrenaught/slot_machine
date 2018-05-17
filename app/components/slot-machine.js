269c48d8cedca671429325d320ebf3acf5ff1ebc
KimSaeHun <kimsaehun@csu.fullerton.edu>
2018-05-16 18:47:56 -0700

    Add code for udpateing credit scroe

--- app/components/slot-machine.js
+++ app/components/slot-machine.js
@@ -4,18 +4,22 @@ import {
   set
 } from '@ember/object';
 
+let RESULTS = [];
 const NUMBER_OF_REELS = 9;
+let SELECTED_LEVEL = 3;
+let CREDITS = 0;
 
 export default Component.extend({
-  credits: 0,
-  selectedLevel: 3,
-
+  mCredits: 0,
   // At this point jQuery onWindow load has completed
   // Calling this makes sure that only 3 slot reel icons are showing
   didInsertElement: function() {
-    for (let i=1; i<=NUMBER_OF_REELS; i++) {
-      this.setReel(`reel${i}`, i*50);
+    for (let i=1; i<NUMBER_OF_REELS+1; i++) {
+      this.setReel(`reel${i}`, i*36);
     }
+  },
+  didRender: function() {
+    this.set('mCredits', CREDITS);
   },
   setReel(name, delay) {
     let reel = this.$(`.${name}`).slotMachine({
@@ -27,114 +31,112 @@ export default Component.extend({
     });
     set(this, name, reel);
   },
-  checkWin() {
-    console.log(`level: ${this.get('selectedLevel')}`);
-    let credits = 0;
-    if (this.selectedLevel >= 1) {
-      // check 2nd row
-      if (((this.get('reel4').active == this.get('reel5').active) && (this.get('reel5').active == this.get('reel6').active))) {
-        if (this.get('reel4').active == 0 || this.get('reel4').active == 1 || this.get('reel4').active == 2)
-          credits += 100;
-        else if (this.get('reel4').active == 3 || this.get('reel4').active == 4)
-          credits += 300;
-        else if (this.get('reel4').active == 5)
-          credits += 1000;
-      }
-    }
-    if (this.selectedLevel >= 2) {
-      // check 1st row
-      if (((this.get('reel1').active == this.get('reel2').active) && (this.get('reel2').active == this.get('reel3').active))) {
-        if (this.get('reel1').active == 0 || this.get('reel1').active == 1 || this.get('reel1').active == 2)
-          credits += 100;
-        else if (this.get('reel1').active == 3 || this.get('reel1').active == 4)
-          credits += 300;
-        else if (this.get('reel1').active == 5)
-          credits += 1000;
-      }
-      // check 3rd row
-      if (((this.get('reel7').active == this.get('reel8').active) && (this.get('reel8').active == this.get('reel9').active))) {
-        if (this.get('reel7').active == 0 || this.get('reel7').active == 1 || this.get('reel7').active == 2)
-          credits += 100;
-        else if (this.get('reel7').active == 3 || this.get('reel7').active == 4)
-          credits += 300;
-        else if (this.get('reel7').active == 5)
-          credits += 1000;
-      }
-    }
-    if (this.selectedLevel >= 3) {
-      // check top-bot diagonal
-      if (((this.get('reel1').active == this.get('reel5').active) && (this.get('reel5').active == this.get('reel9').active))) {
-        if (this.get('reel1').active == 0 || this.get('reel1').active == 1 || this.get('reel1').active == 2)
-          credits += 100;
-        else if (this.get('reel1').active == 3 || this.get('reel1').active == 4)
-          credits += 300;
-        else if (this.get('reel1').active == 5)
-          credits += 1000;
-      }
-      // check bot-top diagonal
-      if (((this.get('reel7').active == this.get('reel5').active) && (this.get('reel5').active == this.get('reel3').active))) {
-        if (this.get('reel7').active == 0 || this.get('reel7').active == 1 || this.get('reel7').active == 2)
-          credits += 100;
-        else if (this.get('reel7').active == 3 || this.get('reel7').active == 4)
-          credits += 300;
-        else if (this.get('reel7').active == 5)
-          credits += 1000;
-      }
-    }
-    this.winCredits(credits)
-  },
-  winCredits(amount) {
-    console.log(`won ${amount}`);
-    // add credits to backend
-    var i;
-    for (i = 0; i < amount; i++) {
-      this.set('credits', this.get('credits') + 1);
-    }
-  },
-  unhighlightAllLevelButtons() {
-    this.$('.level-one-container button').css('background-color', 'rgba(0, 0, 0, 0.12)');
-    this.$('.level-two-container button').css('background-color', 'rgba(0, 0, 0, 0.12)');
-    this.$('.level-three-container button').css('background-color', 'rgba(0, 0, 0, 0.12)');
-  },
-  highlightLevelButtons(level) {
-    if (level == 1) {
-      this.$('.level-one-container button').css('background-color', '#fafafa');
-    }
-    if (level == 2) {
-      this.$('.level-two-container button').css('background-color', '#fafafa');
-    }
-    if (level == 3) {
-      this.$('.level-three-container button').css('background-color', '#fafafa');
-    }
-  },
+  onComplete(active) {
+    RESULTS.push(active);
+    if (RESULTS.length == NUMBER_OF_REELS) {
+      console.log(`Results: ${RESULTS}` + SELECTED_LEVEL);
 
+      //if (betting = 1x)
+      if ((RESULTS[3] == RESULTS[4]) && (RESULTS[4] == RESULTS[5]))
+      {
+        if (RESULTS[3] == 0 || RESULTS[3] == 1 || RESULTS[3] == 2)
+          CREDITS += 100;
+        else if (RESULTS[3] == 3 || RESULTS[3] == 4)
+          CREDITS += 300;
+        else if (RESULTS[3] == 5)
+          CREDITS += 1000;
+      }
+      //if (betting = 2x)
+        else if (((RESULTS[0] == RESULTS[1]) && (RESULTS[1] == RESULTS[2])) || ((RESULTS[3] == RESULTS[4]) && (RESULTS[4] == RESULTS[5])) || ((RESULTS[6] == RESULTS[7]) && (RESULTS[7] == RESULTS[8])))
+        {
+          if (((RESULTS[0] == 0) && (RESULTS[1] == 0) && (RESULTS[2] == 0)) || ((RESULTS[0] == 1) && (RESULTS[1] == 1) && (RESULTS[2] == 1)) || ((RESULTS[0] == 2) && (RESULTS[1] == 2) && (RESULTS[2] == 2)))
+          CREDITS += 100;
+          //Add 100
+          if (((RESULTS[3] == 0) && (RESULTS[4] == 0) && (RESULTS[5] == 0)) || ((RESULTS[3] == 1) && (RESULTS[4] == 1) && (RESULTS[5] == 1)) || ((RESULTS[3] == 2) && (RESULTS[4] == 2) && (RESULTS[5] == 2)))
+            CREDITS += 100;
+          if (((RESULTS[6] == 0) && (RESULTS[7] == 0) && (RESULTS[8] == 0)) || ((RESULTS[6] == 1) && (RESULTS[7] == 1) && (RESULTS[8] == 1)) || ((RESULTS[6] == 2) && (RESULTS[7] == 2) && (RESULTS[8] == 2)))
+            CREDITS += 100;
+          if (((RESULTS[0] == 3) && (RESULTS[1] == 3) && (RESULTS[2] == 3)) || ((RESULTS[0] == 4) && (RESULTS[1] == 4) && (RESULTS[2] == 4)))
+            CREDITS += 300;
+          if (((RESULTS[3] == 3) && (RESULTS[4] == 3) && (RESULTS[5] == 3)) || ((RESULTS[3] == 4) && (RESULTS[4] == 4) && (RESULTS[5] == 4)))
+            CREDITS += 300;
+          if (((RESULTS[6] == 3) && (RESULTS[7] == 3) && (RESULTS[8] == 3)) || ((RESULTS[6] == 4) && (RESULTS[7] == 4) && (RESULTS[8] == 4)))
+            CREDITS += 300;
+          if ((RESULTS[0] == 5) && (RESULTS[1] == 5) && (RESULTS[2] == 5))
+            CREDITS += 1000;
+          if ((RESULTS[3] == 5) && (RESULTS[4] == 5) && (RESULTS[5] == 5))
+            CREDITS += 1000;
+          if ((RESULTS[6] == 5) && (RESULTS[7] == 5) && (RESULTS[8] == 5))
+            CREDITS += 1000;
+        }
+      //if (betting = 3x)
+        else if (((RESULTS[0] == RESULTS[1]) && (RESULTS[1] == RESULTS[2])) || ((RESULTS[3] == RESULTS[4]) && (RESULTS[4] == RESULTS[5])) || ((RESULTS[6] == RESULTS[7]) && (RESULTS[7] == RESULTS[8])) || ((RESULTS[0] == RESULTS[4]) && (RESULTS[0] == RESULTS[8])) || ((RESULTS[2] == RESULTS[4]) && (RESULTS[2] == RESULTS[6])))
+        {
+          if (((RESULTS[0] == 0) && (RESULTS[1] == 0) && (RESULTS[2] == 0)) || ((RESULTS[0] == 1) && (RESULTS[1] == 1) && (RESULTS[2] == 1)) || ((RESULTS[0] == 2) && (RESULTS[1] == 2) && (RESULTS[2] == 2)))
+            CREDITS += 100;
+            //Add 100
+          if (((RESULTS[3] == 0) && (RESULTS[4] == 0) && (RESULTS[5] == 0)) || ((RESULTS[3] == 1) && (RESULTS[4] == 1) && (RESULTS[5] == 1)) || ((RESULTS[3] == 2) && (RESULTS[4] == 2) && (RESULTS[5] == 2)))
+            CREDITS += 100;
+          if (((RESULTS[6] == 0) && (RESULTS[7] == 0) && (RESULTS[8] == 0)) || ((RESULTS[6] == 1) && (RESULTS[7] == 1) && (RESULTS[8] == 1)) || ((RESULTS[6] == 2) && (RESULTS[7] == 2) && (RESULTS[8] == 2)))
+            CREDITS += 100;
+          if (((RESULTS[0] == 0) && (RESULTS[4] == 0) && (RESULTS[8] == 0)) || ((RESULTS[0] == 1) && (RESULTS[4] == 1) && (RESULTS[8] == 1)) || ((RESULTS[0] == 2) && (RESULTS[4] == 2) && (RESULTS[8] == 2)))
+            CREDITS += 100;
+          if (((RESULTS[2] == 0) && (RESULTS[4] == 0) && (RESULTS[6] == 0)) || ((RESULTS[2] == 1) && (RESULTS[4] == 1) && (RESULTS[6] == 1)) || ((RESULTS[2] == 2) && (RESULTS[4] == 2) && (RESULTS[6] == 2)))
+            CREDITS += 100;
+          if (((RESULTS[0] == 3) && (RESULTS[1] == 3) && (RESULTS[2] == 3)) || ((RESULTS[0] == 4) && (RESULTS[1] == 4) && (RESULTS[2] == 4)))
+            CREDITS += 300;
+          if (((RESULTS[3] == 3) && (RESULTS[4] == 3) && (RESULTS[5] == 3)) || ((RESULTS[3] == 4) && (RESULTS[4] == 4) && (RESULTS[5] == 4)))
+            CREDITS += 300;
+          if (((RESULTS[6] == 3) && (RESULTS[7] == 3) && (RESULTS[8] == 3)) || ((RESULTS[6] == 4) && (RESULTS[7] == 4) && (RESULTS[8] == 4)))
+            CREDITS += 300;
+          if (((RESULTS[0] == 3) && (RESULTS[4] == 3) && (RESULTS[8] == 3)) || ((RESULTS[0] == 4) && (RESULTS[4] == 4) && (RESULTS[8] == 4)))
+            CREDITS += 300;
+          if (((RESULTS[2] == 3) && (RESULTS[4] == 3) && (RESULTS[6] == 3)) || ((RESULTS[2] == 4) && (RESULTS[4] == 4) && (RESULTS[6] == 4)))
+            CREDITS += 300;
+          if ((RESULTS[0] == 5) && (RESULTS[1] == 5) && (RESULTS[2] == 5))
+            CREDITS += 1000;
+          if ((RESULTS[3] == 5) && (RESULTS[4] == 5) && (RESULTS[5] == 5))
+            CREDITS += 1000;
+          if ((RESULTS[6] == 5) && (RESULTS[7] == 5) && (RESULTS[8] == 5))
+            CREDITS += 1000;
+          if ((RESULTS[0] == 5) && (RESULTS[4] == 5) && (RESULTS[8] == 5))
+            CREDITS += 1000;
+          if ((RESULTS[2] == 5) && (RESULTS[4] == 5) && (RESULTS[6] == 5))
+            CREDITS += 1000;
+        }
+    }
+    console.log("inside "+ CREDITS);
+  },
   actions: {
     // call to spin the slot reels
     shuffle() {
-      for (let i=1; i<=NUMBER_OF_REELS; i++) {
-//
+      // reset reel for start
+      RESULTS = [];
+      for (let i=1; i<NUMBER_OF_REELS+1; i++) {
         get(this, `reel${i}`).shuffle(99999, this.onComplete);
       }
+      CREDITS -= 10;
+      this.set('mCredits', CREDITS);
     },
     stop() {
-      for (let i=1; i<=NUMBER_OF_REELS; i++) {
+      for (let i=1; i<NUMBER_OF_REELS+1; i++) {
         get(this, `reel${i}`).shuffle(0, this.onComplete);
       }
-      this.checkWin();
+      console.log("outside " + CREDITS); // displays the creidts
     },
     selectLevelOne() {
-      this.selectedLevel = 1;
+      SELECTED_LEVEL = 1;
       this.unhighlightAllLevelButtons();
       this.highlightLevelButtons(1);
     },
     selectLevelTwo() {
-      this.selectedLevel = 2;
+      SELECTED_LEVEL = 2;
       this.unhighlightAllLevelButtons();
       this.highlightLevelButtons(1);
       this.highlightLevelButtons(2);
     },
     selectLevelThree() {
-      this.selectedLevel = 3;
+      SELECTED_LEVEL = 3;
       this.unhighlightAllLevelButtons();
       this.highlightLevelButtons(1);
       this.highlightLevelButtons(2);
